1574883a922022f87913528a5607288ce2f8318b
hansenhsu <hansenhsu@gmail.com>
2024-07-26 15:01:11 -0700

    Added JFrame

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        JFrame frame = new JFrame("Camel Rush");
+        frame.setSize(600,600);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
