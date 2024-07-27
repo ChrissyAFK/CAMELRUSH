3e1329cd0fd92529c721d584956179df21818dae
ChrissyAFK <cshen0800@icloud.com>
2024-07-26 17:46:18 -0700

    fix leaderboard

--- src/leaderboard.java
+++ src/leaderboard.java
@@ -3,14 +3,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 public class leaderboard {
     public static void main(String[] args) {
-        String filepath = "local/leaderboard.csv";
+        String filepath = "CAMELRUSH/local/leaderboard.csv";
         File file = new File(filepath);
         try {
             if (file.createNewFile()) {
                 System.out.println("File created: " + file.getName());
-                insertdata("bob", "1:00");
+                insertdata("tom", "1:00");
             } else {
-                System.out.println("File already exists.");
+                insertdata("jerry", "2:00");
             }
         } catch (IOException e) {
             System.out.println("An error occurred.");
@@ -18,7 +18,7 @@ public class leaderboard {
 
 }
 public static void insertdata(String name, String time) throws IOException{
-    FileWriter writer = new FileWriter("local/leaderboard.csv", true);
+    FileWriter writer = new FileWriter("CAMELRUSH/local/leaderboard.csv", true);
     writer.write(name+", "+time+"\n");
     writer.close();
     System.out.println("Successfully wrote to the file.");
