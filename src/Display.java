79e79194a8e111ad036c9a46f8299fb21319bafa
hansenhsu <hansenhsu@gmail.com>
2024-07-27 01:24:48 -0700

    added fps counter
    added to display file

--- src/Display.java
+++ src/Display.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.util.ArrayList;
 
@@ -16,15 +17,21 @@ public class Display extends JPanel {
 	private int playerSpeed = 2;
 	private int frameCount;
 	private double fps;
-	private long delta;
 	private long startTime;
 	private long prevTime;
-	private long currentTime;
 	private JLabel fpsCounter;
 	
 	Display(InputHandler input,Player player) {
 		this.setPreferredSize(new Dimension(600,600));
 		setLayout(null); //44x32
+		
+		this.startTime = System.currentTimeMillis();
+		this.prevTime = this.startTime;
+		this.fpsCounter = new JLabel("");
+		this.fpsCounter.setFont(new Font("Dialog",Font.BOLD,20));
+		this.fpsCounter.setBounds(10,10,120,20);
+		this.add(fpsCounter);
+		
 		this.animateCamel = new Timer((1000/15),e->animate());
 		this.animateCamel.start();
 		this.scrollTimer = new Timer(1,e->scroll());
@@ -32,30 +39,24 @@ public class Display extends JPanel {
 		this.input = input;
 		this.player = player;
 		this.tile = new TileCalculator();
-		
-		this.startTime = System.currentTimeMillis();
-		this.prevTime = this.startTime;
-		this.fpsCounter = new JLabel("");
-		this.fpsCounter.setBounds(10,10,120,20);
-		this.add(fpsCounter);
 	}
 	
 	private void animate() {
 		this.player.changeCamelAnimation();
-		this.currentTime = System.currentTimeMillis();
-		this.delta = this.currentTime - this.prevTime;
-		this.prevTime = this.currentTime;
-		this.frameCount++;
-		this.fpsCounter.setText(String.valueOf(this.fps));
-		if (this.frameCount%10==0) {
-			this.fps = this.frameCount/this.delta;
-			this.startTime = System.currentTimeMillis();
-			this.frameCount = 0;
-		}
 		repaint();
 	}
 	
 	private void scroll() {
+		long currentTime = System.currentTimeMillis();
+		//long delta = currentTime - this.prevTime;
+		this.prevTime = currentTime;
+		this.frameCount++;
+		this.fpsCounter.setText("FPS: "+String.valueOf((double)Math.round(this.fps*100)/100));
+		if (currentTime-this.startTime>1000) {
+			this.fps = (double)this.frameCount/((currentTime-this.startTime)/1000.0);
+			this.startTime = System.currentTimeMillis();
+			this.frameCount = 0;
+		}
 		if (this.input.wKeyPressed()) {
 			Player.changeYcoord(this.playerSpeed);
 		}
@@ -90,9 +91,9 @@ public class Display extends JPanel {
 			for (int j=0;j<tileList.get(i).length;j++) {
 				//g.drawImage(this.tile.getSandTile(),this.tile.getOffset()+50*(i-3),400-50*j,50,50,this);
 				if (tileList.get(i)[j].equals("S")) {
-					g.drawImage(this.tile.getSandTile(),j*50-Player.getCoordinates()[0],i*50-150-Player.getCoordinates()[1],50,50,this);
+					g.drawImage(this.tile.getSandTile(),j*50-Player.getCoordinates()[0],i*50-150+Player.getCoordinates()[1],50,50,this);
 				} else if (tileList.get(i)[j].equals("W")) {
-					g.drawImage(this.tile.getWaterTile(),j*50-Player.getCoordinates()[0],i*50-150-Player.getCoordinates()[1],50,50,this);
+					g.drawImage(this.tile.getWaterTile(),j*50-Player.getCoordinates()[0],i*50-150+Player.getCoordinates()[1],50,50,this);
 				}
 			}
 		}
