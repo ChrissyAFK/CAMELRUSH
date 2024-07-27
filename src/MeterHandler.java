9a254bf942aeeb80f638cfb1f82048215dd21cff
Pengwuin <149349271+PengwuinReal@users.noreply.github.com>
2024-07-26 19:26:23 -0700

    Heat rises, dies at 10m

--- src/MeterHandler.java
+++ src/MeterHandler.java
@@ -1,14 +1,9 @@
-import javax.swing.Timer;
+import java.util.Timer;
+import java.util.TimerTask;
 public class MeterHandler {
     public static void main(String[] args) {
         WaterMeter water = new WaterMeter();
-        OverheatMeter heat = new OverheatMeter();
-        
-            if (Player.inSun()){ //should be changed to Player.inSun() 
-                heat.setAmount(heat.getAmount()+1);
-                System.out.println("heat: "+heat.getAmount());
-            }
-        
+        OverheatMeter heat = new OverheatMeter();        
     }
 }
 class WaterMeter{ //obj name: water
@@ -26,10 +21,17 @@ class WaterMeter{ //obj name: water
 }
 class OverheatMeter{ //obj name: heat
     private float amount = 0.0f;
-    static float max_amount = 100.0f;
+    static float max_amount = 600.0f;//seconds
     Timer heat_rise_delay;
     OverheatMeter(){
-        this.heat_rise_delay = new Timer((1000/50));
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                heatRise(1f);
+            }
+        };
+        this.heat_rise_delay = new Timer();
+        this.heat_rise_delay.schedule(timerTask, 1000, 1000);
     }
     float getAmount(){
         return this.amount;
@@ -39,5 +41,15 @@ class OverheatMeter{ //obj name: heat
     }
     void setAmount(float amount){
         this.amount = amount;
+    }
+    private void heatRise(float amount){
+        if (true){ //change to Player.inSun()
+            this.setAmount(this.getAmount()+amount);
+            //System.out.println("heat: "+this.getAmount()); // debugging
+        }
+        if (this.getAmount() >= this.getMax()){
+            System.out.println("Player has died."); // change this into a method which tells the player its dead
+            this.heat_rise_delay.cancel();
+        }
     }
 }
