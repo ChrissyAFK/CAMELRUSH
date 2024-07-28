6bbd3b55b036f08d50ff6a0cf06cc67396ea921b
Pengwuin <149349271+PengwuinReal@users.noreply.github.com>
2024-07-27 19:35:51 -0700

    real one

--- src/CollisionHandler.java
+++ src/CollisionHandler.java
@@ -1,27 +1,38 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class CollisionHandler {
-	private ArrayList<int[]> tile_hitboxes;
-	private int[] player_hitbox;
-	public static void initializeHitboxes() throws Exception{
+	private ArrayList<Tile> tile_hitboxes;
+	private int[] player_body = {}; //top left, top right, bottom left, bottom right
+	private int[] player_head;
+	public void initializeHitboxes() throws Exception{
 		ArrayList<String[]> room = new TileCalculator().getViewingSlice();
 		for (int j=0;j<room.get(0).length;j++) {
 			for (int i=0;i<room.size();i++) {
-				
+				if (!room.get(j)[i].equals("O")){
+					tile_hitboxes.add(new Tile(i*50-Player.getCoordinates()[0],j*50-150+Player.getCoordinates()[1], room.get(j)[i]));
+				}
 			}
 		}
 	}
-	private class tile{
-		private int x1;
-		private int x2;
-		private int y1;
-		private int y2;
+	public List<Tile> isTouching(){
+		for (int j=0;j<tile_hitboxes.get(0).length;j++) {
+			for (int i=0;i<tile_hitboxes.size();i++) {
+				if (i<Player.getCoordinates()[0]/50+15 && i>Player.getCoordinates()[0]/50-3){
+					
+				}
+			}
+		}
+	}
+	public class Tile{
+		public int x;
+		public int y;
+		public static final int width = 50;
+		public static final int height = 50;
 		public String type; 
-		public tile(int x1, int x2, int y1, int y2, String type){
-			this.x1 = x1;
-			this.x2 = x2;
-			this.y1 = y1;
-			this.y2 = y2;
+		public Tile(int x, int y, String type){
+			this.x = x;
+			this.y = y;
 			this.type = type;
 		}
 	}
